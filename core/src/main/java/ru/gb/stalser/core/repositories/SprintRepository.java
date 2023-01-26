56d1a277b82a2516be7b9e3de23adf529262a54a
celean77777 <celean77777@gmail.com>
2023-01-27 00:02:12 +0300

    SAS12

---
+++ core/src/main/java/ru/gb/stalser/core/repositories/SprintRepository.java
@@ -0,0 +1,7 @@
+package ru.gb.stalser.core.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.gb.stalser.core.entity.Sprint;
+
+public interface SprintRepository extends JpaRepository<Sprint, Long> {
+}
