359a603f6eb57dca4163b5b63286b96c8e1e9471
Stanislav <grafwwwolf@yandex.ru>
2023-01-26 19:56:25 +0300

    доработки по комментам

--- core/src/main/resources/db/migration/V2__update_boards_table.sql
+++ core/src/main/resources/db/migration/V2__update_boards_table.sql
@@ -1,5 +1,5 @@
 ALTER TABLE boards
-ADD COLUMN IF NOT EXISTS board_alias varchar(10) NOT NULL DEFAULT 'board',
+    ADD COLUMN IF NOT EXISTS board_alias varchar(10) NOT NULL DEFAULT 'board',
 ADD COLUMN IF NOT EXISTS is_active boolean NOT NULL DEFAULT true,
-ADD COLUMN IF NOT EXISTS creator_id bigserial,
+ADD COLUMN IF NOT EXISTS creator_id bigint,
 ADD CONSTRAINT fk_board_creator_id FOREIGN KEY (creator_id) REFERENCES users (id);
