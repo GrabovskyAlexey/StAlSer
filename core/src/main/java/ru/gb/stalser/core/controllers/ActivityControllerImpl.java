ec707f23032a180892d8780ed71215316195f4ac
Kingl <siarheitsarik@gmail.com>
2023-01-29 23:31:58 +0300

    Добавил связи в сущность "активности". Добавлен контроллер для активностей со swagger аннотациями, добавлен mapper и ДТО.

---
+++ core/src/main/java/ru/gb/stalser/core/controllers/ActivityControllerImpl.java
@@ -0,0 +1,45 @@
+package ru.gb.stalser.core.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.gb.stalser.api.dto.activity.ActivityDto;
+import ru.gb.stalser.core.controllers.interfaces.ActivityController;
+import ru.gb.stalser.core.mappers.ActivityMapper;
+import ru.gb.stalser.core.services.interfaces.ActivityService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/${stalser.api.url}/activity")
+@RequiredArgsConstructor
+public class ActivityControllerImpl implements ActivityController {
+    private final ActivityService activityService;
+    private final ActivityMapper activityMapper;
+    @Override
+    public ResponseEntity<List<ActivityDto>> getAllActivities() {
+        return ResponseEntity.ok(activityService.findAll().stream().map(activityMapper::mapToDto).collect(Collectors.toList()));
+    }
+
+    @Override
+    public ResponseEntity<ActivityDto> getActivityById(Long id) {
+        return ResponseEntity.ok(activityMapper.mapToDto(activityService.findById(id)));
+    }
+
+    @Override
+    public ResponseEntity<ActivityDto> addActivity(ActivityDto activity) {
+        return ResponseEntity.ok(activityMapper.mapToDto(activityService.save(activityMapper.mapFromDto(activity))));
+    }
+
+    @Override
+    public ResponseEntity<ActivityDto> updateActivity(Long id, ActivityDto activity) {
+        return ResponseEntity.ok(activityMapper.mapToDto(activityService.update(activityMapper.mapFromDto(activity))));
+    }
+
+    @Override
+    public void deleteActivity(Long id) {
+        activityService.deleteById(id);
+    }
+}
--- core/src/main/java/ru/gb/stalser/core/entity/Activity.java
+++ core/src/main/java/ru/gb/stalser/core/entity/Activity.java
@@ -8,7 +8,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.Instant;
-import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "task_activities")
@@ -27,6 +27,14 @@ public class Activity {
     @Column(name = "activity_info")
     private String activityInfo;
 
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id")
+    private Task task;
+
     @Column(name = "created_at")
     @CreationTimestamp
     private Instant createdAt;
@@ -34,4 +42,17 @@ public class Activity {
     @UpdateTimestamp
     @Column(name = "updated_at")
     private Instant updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Activity activity = (Activity) o;
+        return id.equals(activity.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
