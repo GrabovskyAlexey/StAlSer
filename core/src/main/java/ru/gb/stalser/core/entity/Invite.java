c1bfeff15ec29c59d6c65f60a84f6b5b576074fb
Kingl <siarheitsarik@gmail.com>
2023-01-30 22:09:11 +0300

    SAS-21. Добавлена сущность для приглашений и репозиторий.

---
+++ core/src/main/java/ru/gb/stalser/core/entity/Invite.java
@@ -0,0 +1,63 @@
+package ru.gb.stalser.core.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "invites")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Invite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "invite_code")
+    private String inviteCode;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "expiration_date")
+    private Instant expirationDate;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Invite invite = (Invite) o;
+        return id.equals(invite.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
