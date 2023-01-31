44c82dc3381bab2765b24f33db8d5eb040562103
LowBob93 <Tester269@gmail.com>
2023-01-31 14:30:06 +0300

    Протер глаза, исправил)

--- core/src/main/java/ru/gb/stalser/core/repositories/CommentRepository.java
+++ core/src/main/java/ru/gb/stalser/core/repositories/CommentRepository.java
@@ -1,11 +1,11 @@
 package ru.gb.stalser.core.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.gb.stalser.core.entity.Role;
+import ru.gb.stalser.core.entity.Comment;
 
 import java.util.List;
 
-public interface CommentRepository extends JpaRepository<Role,Long> {
+public interface CommentRepository extends JpaRepository<Comment,Long> {
 
 
 }
--- core/src/main/java/ru/gb/stalser/core/repositories/RoleRepository.java
+++ core/src/main/java/ru/gb/stalser/core/repositories/RoleRepository.java
@@ -1,8 +1,9 @@
 package ru.gb.stalser.core.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.gb.stalser.core.entity.Comment;
+import ru.gb.stalser.core.entity.Role;
 
-public interface RoleRepository  extends JpaRepository<Comment,Long> {
+
+public interface RoleRepository  extends JpaRepository<Role,Long> {
 
 }
