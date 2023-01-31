7920812e0e496fa7acaefd6b15097b4a395b47e1
LowBob93 <Tester269@gmail.com>
2023-01-31 14:22:05 +0300

    Убираем копипасту)

--- core/src/main/java/ru/gb/stalser/core/repositories/CommentRepository.java
+++ core/src/main/java/ru/gb/stalser/core/repositories/CommentRepository.java
@@ -1,11 +1,11 @@
 package ru.gb.stalser.core.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.gb.stalser.core.entity.Comment;
+import ru.gb.stalser.core.entity.Role;
 
 import java.util.List;
 
-public interface CommentRepository extends JpaRepository<Comment,Long> {
+public interface CommentRepository extends JpaRepository<Role,Long> {
 
 
 }
