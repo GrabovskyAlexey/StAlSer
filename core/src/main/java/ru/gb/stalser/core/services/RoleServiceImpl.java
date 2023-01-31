936855379aae136943d4281ba7473dee47c4697b
LowBob93 <Tester269@gmail.com>
2023-01-31 18:29:33 +0300

    Добавлен сервис для "Ролей"

---
+++ core/src/main/java/ru/gb/stalser/core/services/RoleServiceImpl.java
@@ -0,0 +1,44 @@
+package ru.gb.stalser.core.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.gb.stalser.core.entity.Role;
+import ru.gb.stalser.core.repositories.RoleRepository;
+import ru.gb.stalser.core.services.interfaces.RoleService;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+
+
+    @Override
+    public List<Role> findAll() {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public Role findById(Long id) {
+        return roleRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Role id - " + id + " not found"));
+    }
+
+    @Override
+    public Role save(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public Role update(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        roleRepository.deleteById(id);
+    }
+}
