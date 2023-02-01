037dc90429d1205492f6d82ceb3a1b87ff131e36
Stanislav <grafwwwolf@yandex.ru>
2023-02-01 19:34:38 +0300

    SAS-27 Создание сервиса пользователей (с поддержкой SpringSecurity)

---
+++ core/src/main/java/ru/gb/stalser/core/services/UserServiceImpl.java
@@ -0,0 +1,73 @@
+package ru.gb.stalser.core.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.gb.stalser.core.entity.Role;
+import ru.gb.stalser.core.entity.User;
+import ru.gb.stalser.core.repositories.UserRepository;
+import ru.gb.stalser.core.services.interfaces.RoleService;
+import ru.gb.stalser.core.services.interfaces.UserService;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final RoleService roleService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("User id=" + id + "{} not found"));
+    }
+
+    @Override
+    public User findByLogin(String login) {
+        return userRepository.findByLogin(login).orElseThrow(
+                () -> new EntityNotFoundException("User login=" + login + "{} not found"));
+    }
+
+    @Override
+    public User save(User user) {
+        Role role = roleService.findByName("ROLE_USER").orElseThrow(() -> new UsernameNotFoundException("Role: \"ROLE_USER\" not found"));
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles(new ArrayList<>());
+        user.getRoles().add(role);
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByLogin(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("User '%s' not found", username)));
+        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getRoleName())).collect(Collectors.toList());
+    }
+}
