064cfcf45246586bf8a6c5ee252b9396c52e16ff
GrabovskyAlexey <grabovsky.alexey@gmail.com>
2023-02-02 22:18:54 +0300

    SAS-24 fix builder annotation

--- api/src/main/java/ru/gb/stalser/api/dto/notify/SimpleTextEmailMessage.java
+++ api/src/main/java/ru/gb/stalser/api/dto/notify/SimpleTextEmailMessage.java
@@ -3,6 +3,7 @@ package ru.gb.stalser.api.dto.notify;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.experimental.SuperBuilder;
 
 /**
  * Info to create simple text email message
@@ -10,7 +11,7 @@ import lombok.EqualsAndHashCode;
  * @author GrabovskyAlexey
  */
 @Data
-@Builder
+@SuperBuilder
 @EqualsAndHashCode(callSuper = true)
 public class SimpleTextEmailMessage extends EmailMessage{
     private String text;
