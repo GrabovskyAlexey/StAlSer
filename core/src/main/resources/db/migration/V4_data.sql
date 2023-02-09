40bb134ce5c7c7d14979876fedc2d8c068f56551
celean77777 <celean77777@gmail.com>
2023-02-09 20:05:17 +0300

    SAS-50

--- core/src/main/resources/db/migration/V4_data.sql
+++ core/src/main/resources/db/migration/V4_data.sql
@@ -1,6 +1,6 @@
 insert into roles (role_name, role_desc)
-values ('ROLE_USER', 'ROLE_USER'),
-       ('ROLE_ADMIN', 'ROLE_ADMIN');
+values ('ROLE_USER', 'Пользователь'),
+       ('ROLE_ADMIN', 'Администратор');
 
 insert into users (emale, login, password)
 values ('test@gmail.com', 'user', '$2a$12$44Ob4I8FtyfgfrdyUfrMQ.JuBogZTSxt5fawiUcWPoMsDrwAZL.Me');
