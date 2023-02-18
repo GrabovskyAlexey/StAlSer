25ad7d89cddb21a82b7574fc8e97b2ef401522e4
Stanislav <grafwwwolf@yandex.ru>
2023-02-18 14:12:20 +0300

    настройка qa модуля

--- qa_automation/src/test/java/ru/stalser/TestBase.java
+++ qa_automation/src/test/java/ru/stalser/TestBase.java
@@ -1,27 +1,25 @@
 package ru.stalser;
 
+import io.restassured.RestAssured;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import ru.stalser.framework.helpers.AllureHelper;
+import ru.stalser.framework.helpers.RestAssuredLoggingFilter;
 import ru.stalser.framework.helpers.SoftAssertHelper;
 import ru.stalser.framework.helpers.StepsHierarchy;
 import ru.stalser.framework.utils.Props;
+
+import java.util.Collections;
 
 import static ru.stalser.framework.helpers.SoftAssertHelper.softly;
 
 @Slf4j
 public class TestBase {
 
-//    protected static final ThreadLocal<SoftAssertions> softAssertions = new ThreadLocal<>();
     protected static final ThreadLocal<TestBase> thisTest = new ThreadLocal<>();
-
-//    public static SoftAssertions softly() {
-//        return softAssertions.get();
-//    }
 
     public static TestBase getThisTest() {
         return thisTest.get();
@@ -30,6 +28,8 @@ public class TestBase {
     @BeforeAll
     public static void beforeAll() {
         Props.init();
+        RestAssured.reset();
+        RestAssured.filters(Collections.singletonList(new RestAssuredLoggingFilter()));
         log.info(Thread.currentThread().getName() + ":: Инициализация завершена");
     }
 
@@ -57,7 +57,7 @@ public class TestBase {
     @AfterAll
     public static void afterAll() {
 
-//        unsubscribeAll();  TODO
+//        unsubscribeAll();  TODO для кафки
     }
 
 }
